7192028d98ae47c75e91dd5276602c77079ae2f6
camera <petit7268@gmail.com>
2023-12-24 08:27:17 -0500

    camera now work, second page added

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder  # fait le lien avec le fichier .kv
 # need to install Package opencv-python manually
-
+import time
 import requests
 
 Builder.load_file('frontend.kv')
@@ -14,12 +14,22 @@ Builder.load_file('frontend.kv')
 class CameraScreen(Screen):
     def start(self):
         self.ids.camera.play = True
+        self.ids.camera_button.text = 'Stop Camera'
+        self.ids.camera.texture = self.ids.camera._camera.texture
 
     def stop(self):
-        self.ids.camara.play = False
+        self.ids.camera.play = False
+        self.ids.camera_button.text = 'Start Camera'
+        self.ids.camera.texture = None
+        self.ids.camera.texture = None
+
+        pass
 
     def capture(self):
-        pass
+        current_time = time.strftime('%Y%m%d-%H%M%S')
+        file_path = f'files/{current_time}.png'
+        self.ids.camera.export_to_png(file_path)
+        self.manager.current = 'image_screen'
 
 
 class ImageScreen(Screen):
